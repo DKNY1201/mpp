f91d1cac306508fba11b3fd2da712cde0c7ed27a
Quy Tran <quytran288@gmail.com>
2017-06-28 15:46:54 -0500

    Using Composition instead of Inheritance for Person & PersonWithJob

--- src/lab3/prob1_quy/PersonWithJob.java
+++ src/lab3/prob1_quy/PersonWithJob.java
@@ -1,15 +1,15 @@
 package lab3.prob1_quy;
 
-public class PersonWithJob extends Person {
-	
+public class PersonWithJob {
+	Person person;
 	private double salary;
 	
 	public double getSalary() {
 		return salary;
 	}
 	PersonWithJob(String n, double s) {
-		super(n);
-		salary = s;
+		this.person = new Person(n);
+		this.salary = s;
 	}
 	
 	@Override
@@ -17,16 +17,24 @@ public class PersonWithJob extends Person {
 		if(aPerson == null) return false; 
 		if(!(aPerson instanceof PersonWithJob)) return false;
 		PersonWithJob p = (PersonWithJob)aPerson;
-		boolean isEqual = this.getName().equals(p.getName()) &&
+		boolean isEqual = this.person.equals(p.person) &&
 				this.getSalary()==p.getSalary();
 		return isEqual;
 	}
 	public static void main(String[] args) {
-		Person p1 = new PersonWithJob("Joe", 30000);
+		PersonWithJob p1 = new PersonWithJob("Joe", 30000);
 		Person p2 = new Person("Joe");
+		PersonWithJob p3 = new PersonWithJob("Joe", 30000);
+		Person p4 = new Person("Joe");
 		//As PersonsWithJobs, p1 should be equal to p2
 		System.out.println("p1.equals(p2)? " + p1.equals(p2));
 		System.out.println("p2.equals(p1)? " + p2.equals(p1));
+
+		System.out.println("p1.equals(p3)? " + p1.equals(p3));
+		System.out.println("p3.equals(p1)? " + p3.equals(p1));
+
+		System.out.println("p2.equals(p4)? " + p2.equals(p4));
+		System.out.println("p4.equals(p2)? " + p4.equals(p2));
 	}
 
 
