ead3ad1a2f7b33ce18340af484b1bf96b36ea401
Quy Tran <quytran288@gmail.com>
2017-06-27 22:58:52 -0500

    First commit with UML for lab2.prog1

---
+++ src/lab2/prog1/Project.java
@@ -0,0 +1,15 @@
+package lab2.prog1;
+
+import java.util.Date;
+
+/**
+ * Created by Bi on 6/27/17.
+ */
+public class Project {
+    String name;
+    Date releaseDate;
+
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+    }
+}
