25905a40da627ecd5eb421b171a9220f8d016e86
Quy Tran <quytran288@gmail.com>
2017-07-02 14:33:05 -0500

    Finished Lab5-Prob2

---
+++ src/lab5/prob2/QuackBehavior.java
@@ -0,0 +1,8 @@
+package lab5.prob2;
+
+/**
+ * Created by Bi on 7/2/17.
+ */
+public interface QuackBehavior {
+    void quack();
+}
---
+++ src/lab5/prob2/Squeak.java
@@ -0,0 +1,11 @@
+package lab5.prob2;
+
+/**
+ * Created by Bi on 7/2/17.
+ */
+public class Squeak implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("squeaking");
+    }
+}
