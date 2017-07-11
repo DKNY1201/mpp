120b4a659e5f0055e742058a25636795c39d7be7
Quy Tran <quytran288@gmail.com>
2017-07-10 22:48:15 -0500

    Finished lab7-prob1

---
+++ src/lab7/prob1_quy/partB/Employee.java
@@ -0,0 +1,45 @@
+package lab7.prob1_quy.partB;
+
+public class Employee {
+	private String name;
+	private int salary;
+	public Employee(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	@Override 
+	public String toString() {
+		return "(" + name + ", " + salary + ")";
+	}
+	
+//	public boolean equals(Employee e) {
+//		return e.name.equals(name) && e.salary == salary;
+//	}
+
+	public boolean equals(Object o) {
+		if (o == null) {
+			return false;
+		}
+
+		if (o.getClass() != getClass()) {
+			return false;
+		}
+
+		Employee e = (Employee) o;
+
+		return e.name.equals(name) && e.salary == salary;
+	}
+}
---
+++ src/lab7/prob1_quy/partC/Employee.java
@@ -0,0 +1,44 @@
+package lab7.prob1_quy.partC;
+
+import java.util.Objects;
+
+public class Employee {
+	private String name;
+	private int salary;
+	public Employee(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	@Override 
+	public String toString() {
+		return "(" + name + ", " + salary + ")";
+	}
+	
+	@Override
+	public boolean equals(Object ob) {
+		if(ob == null) return false;
+		if(!(ob instanceof Employee)) return false;
+		Employee emp = (Employee)ob;
+		return emp.name.equals(name) && emp.salary == salary;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name,salary);
+	}
+	
+	
+}
