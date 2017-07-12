743fa528b5967d05892b17baeff1d2100adebe0a
Quy Tran <quytran288@gmail.com>
2017-07-12 01:22:10 -0500

    Finished Lab8-Prog5

---
+++ src/lab8/prob5/ForEachExampleMethodReference.java
@@ -0,0 +1,23 @@
+package lab8.prob5;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+public class ForEachExampleMethodReference {
+	
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", 
+				"Away", "On Vacation", "Everywhere you want to be");
+
+		Function<List<String>, List<String>> toUpper = listStr -> listStr
+				.stream()
+				.map(str -> str.toUpperCase())
+				.collect(Collectors.toList());
+		List<String> upperList = toUpper.apply(list);
+		upperList.forEach(System.out::println);
+	}
+
+}
